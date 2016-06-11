46b589c8b3f743805fd0e5ca89b711902d824e6e
John <jlister76@gmail.com>
2016-06-11 17:49:33 -0500

    ...

--- client/issue-reports/issueReportController.js
+++ client/issue-reports/issueReportController.js
@@ -34,7 +34,7 @@
           .then(function(){
             console.log("Confirmation saved");
             $scope.msgStatus = 1;
-            $scope.confirmation = "Thank you. This issue has been reported."
+            $scope.confirmation = "Thank you and may the force be with you."
           });
       }
     })
